8c1bed7b25cd441876076051d4ed0ae3ec164b05
morkbork <markyang645@gmail.com>
2026-03-06 10:38:45 -0600

    Mar 6, 2026, 10:38 AM

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,54 @@ export default {
           900: '#581c87',
           950: '#3b0764',
         },
+        // Friend Group Colors
+        'teal': {
+          DEFAULT: '#008080',
+          light: '#4db8b8',
+          dark: '#004d4d',
+        },
+        'orange': {
+          DEFAULT: '#ff8c00',
+          light: '#ffb84d',
+          dark: '#cc7000',
+        },
+        'vantablack': '#0a0a0a',
+        'periwinkle': '#ccccff',
+        'purple': {
+          DEFAULT: '#9333ea',
+          light: '#c084fc',
+          dark: '#6b21a8',
+        },
+        'green': {
+          DEFAULT: '#22c55e',
+          light: '#4ade80',
+          dark: '#15803d',
+        },
+        'yellow': {
+          DEFAULT: '#eab308',
+          light: '#fde047',
+          dark: '#a16207',
+        },
+        'gold': {
+          DEFAULT: '#ffd700',
+          light: '#ffed4a',
+          dark: '#b8860b',
+        },
+        'sky-blue': {
+          DEFAULT: '#87ceeb',
+          light: '#b8e4f5',
+          dark: '#5ba3c9',
+        },
+        'galaxy-blue': {
+          DEFAULT: '#4b6cb7',
+          light: '#7a94d4',
+          dark: '#2d4a7c',
+        },
+        'blood-red': {
+          DEFAULT: '#8b0000',
+          light: '#c41e1e',
+          dark: '#5c0000',
+        },
       },
     },
   },
